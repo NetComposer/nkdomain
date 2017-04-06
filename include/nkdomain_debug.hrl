82bcb7edd02b2d08f0ca805226bdf6084df56cfc
Carlos Gonzalez <carlosj.gf@gmail.com>
2017-04-06 17:22:09 +0200

    Include nkapi

---
+++ include/nkdomain_debug.hrl
@@ -0,0 +1,88 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2017 Carlos Gonzalez Florido.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-ifndef(NKDOMAIN_HRL_).
+-define(NKDOMAIN_HRL_, 1).
+
+%% ===================================================================
+%% Defines
+%% ===================================================================
+
+-define(ADD_TO_OBJ(Key, Val, Obj), maps:put(Key, Val, Obj)).
+-define(ADD_TO_OBJ(Update, Obj), maps:merge(Obj, Update)).
+-define(ADD_TO_OBJ_DEEP(Update, Obj), nklib_syntax:map_merge(Update, Obj)).
+-define(REMOVE_FROM_OBJ(Key, Obj), maps:remove(Key, Obj)).
+
+-record(obj_session, {
+    obj_id :: nkdomain:obj_id(),
+    module :: module(),
+    type :: binary(),
+    path :: nkdomain:path(),
+    parent_id :: nkdomain:obj_id(),
+    parent_pid :: pid(),
+    obj :: nkdomain:obj(),
+    srv_id :: nkservice:id(),
+    is_dirty :: boolean(),
+    is_enabled :: boolean(),
+    status :: nkdomain_obj:status(),
+    meta :: map(),                      % Object load metadata
+    data :: term()                      % Type-specific metadata
+}).
+
+-record(obj_id_ext, {
+    srv_id :: nkservice:id(),
+    type :: nkdomain:type(),
+    obj_id :: nkdomain:obj_id(),
+    path :: nkdomain:path(),
+    pid :: pid() | undefined
+}).
+
+
+
+
+-define(DOMAIN_USER, <<"user">>).
+-define(DOMAIN_DOMAIN, <<"domain">>).
+-define(DOMAIN_SESSION, <<"session">>).
+-define(DOMAIN_TOKEN, <<"token">>).
+-define(DOMAIN_CONFIG, <<"config">>).
+
+-define(DEBUG_MACROS(Type),
+
+    -define(DEBUG(Txt, Args, State),
+        case erlang:get(object_debug) of
+            true -> ?LLOG(debug, Txt, Args, State);
+            _ -> ok
+        end).
+
+    -define(LLOG(Type, Txt, Args, Session),
+        lager:Type(
+            [
+                {obj_id, Session#obj_session.obj_id},
+                {type, Session#obj_session.type},
+                {path, Session#obj_session.path}
+            ],
+            "NkDOMAIN Obj ~s ~s (~s) " ++ Txt,
+            [Type, Session#obj_session.path, Session#obj_session.obj_id | Args]
+        )).
+    ).
+
+
+-endif.
+
---
+++ src/nkdomain_monitor.erl
@@ -0,0 +1,346 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2017 Carlos Gonzalez Florido.  All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License.  You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied.  See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+%% @doc NkDomain main module
+-module(nkdomain).
+-author('Carlos Gonzalez <carlosj.gf@gmail.com>').
+
+
+-export([find/2, load/2, load/3, create/3]).
+
+-export([load_file/1, export/1, get_aliases/1, get_pid/1, get_obj/1, remove/1]).
+-export([get_roles/1, get_role_objs/2, find_role_objs/2, has_role/3]).
+-export([resolve/1, multi_resolve/1]).
+-export([register_update_callback/3]).
+-export_type([obj_id/0, name/0, obj/0, path/0, type/0, class/0, history/0, history_op/0]).
+
+%%-include_lib("nklib/include/nklib.hrl").
+
+-include("nkdomain.hrl").
+
+%% ===================================================================
+%% Types
+%% ===================================================================
+
+-type obj_id() :: binary().
+
+-type name() :: binary().
+
+-type path() :: [binary()].
+
+-type type() :: binary().
+
+-type class() :: atom().
+
+-type history_op() :: term().
+
+-type history() :: [{nklib_util:m_timestamp(), User::obj_id(), history_op()}].
+
+
+%% @see nkdomain_callbacks:domain_store_base_mapping/0
+-type obj() :: #{
+    obj_id => obj_id(),
+    domain => path(),
+    type => type(),
+    subtype => atom(),
+    description => binary(),
+    created_by => obj_id(),
+    created_time => nklib_util:m_timestamp(),
+    parent_id => obj_id(),
+    enabled => boolean(),
+    expires_time => nklib_util:m_timestamp(),
+    destroyed_time => nklib_util:m_timestamp(),
+    destroyed_reason => term(),
+    icon_id => binary(),
+    aliases => [binary()],
+    service_id => [nkservice:id()],         % Only for service-related objects
+    class() => map()
+}.
+
+
+-type create_opts() ::
+    #{
+        obj_id => obj_id(),
+        register => nklib:link(),
+        events => [nkservice_events:type()],
+        enabled => boolean(),                        % Start disabled
+        remove_after_stop => boolean()
+    }.
+
+-type load_opts() ::
+    #{
+        register => nklib:link()
+    }.
+
+
+%% ===================================================================
+%% Public
+%% ===================================================================
+
+
+
+
+%% ===================================================================
+%% Public
+%% ===================================================================
+
+%% @doc Finds and object from UUID or Path, in memory and disk
+-spec find(nkservice:id(), obj_id()|path()) ->
+    {ok, type(), domain:obj_id(), path(), pid()|undefined} |
+    {error, object_not_found|term()}.
+
+find(Srv, IdOrPath) ->
+    case nkdomain_obj_lib:find(Srv, IdOrPath) of
+        #obj_id_ext{type=Type, obj_id=ObjId, path=Path, pid=Pid} ->
+            {ok, Type, ObjId, Path, Pid};
+        {error, Error} ->
+            {error, Error}
+    end.
+
+
+%% @doc Finds an objects's pid or loads it from storage
+-spec load(nkservice:id(), obj_id()|path()) ->
+    {ok, type(), obj_id(), path(), pid()} |
+    {error, obj_not_found|term()}.
+
+load(Srv, IdOrPath) ->
+    load(Srv, IdOrPath, #{}).
+
+
+%% @doc Finds an objects's pid or loads it from storage
+-spec load(nkservice:id(), obj_id()|path(), load_opts()) ->
+    {ok, type(), obj_id(), path(), pid()} |
+    {error, obj_not_found|term()}.
+
+load(Srv, IdOrPath, Meta) ->
+    case nkdomain_obj_lib:load(Srv, IdOrPath, Meta) of
+        #obj_id_ext{type=Type, obj_id=ObjId, path=Path, pid=Pid} ->
+            {ok, Type, ObjId, Path, Pid};
+        {error, Error} ->
+            {error, Error}
+    end.
+
+
+%% @doc Creates a new object
+-spec create(nkservice:id(), map(), create_opts()) ->
+    {ok, type(), obj_id(), path(), pid()}.
+
+create(Srv, Obj, Meta) ->
+    case nkdomain_obj_lib:create(Srv, Obj, Meta) of
+        #obj_id_ext{type=Type, obj_id=ObjId, path=Path, pid=Pid} ->
+            {ok, Type, ObjId, Path, Pid};
+        {error, Error} ->
+            {error, Error}
+    end.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% @doc Loads a domain configuration from a YAML or JSON file
+-spec load_file(string()|binary()) ->
+    {ok, #{obj_id() => nkdomain_load:load_result()}} | {error, term()}.
+
+load_file(File) ->
+    nkdomain_load:load_file(File, #{token=>admin}).
+
+
+%%%% @doc Loads a domain configuration from an erlang map string or binary
+%%-spec load(map(), nkdomain_load:load_opts()) ->
+%%    {ok, #{obj_id() => nkdomain_load:load_result()}} | {error, term()}.
+%%
+%%load(Data, Opts) ->
+%%    nkdomain_load:load(map, Data, Opts).
+
+
+%% @doc Exports a full domain specification as a map
+-spec export(string()|binary()) ->
+    {ok, map()} | {error, term()} .
+
+export(DomainId) ->
+    nkdomain_obj2:export(domain, DomainId).
+
+
+%% @doc Finds all pointing objects for an alias
+-spec get_aliases(string()|binary()) ->
+    [obj_id()].
+
+get_aliases(Alias) ->
+    case nkdomain_obj2:get_obj(alias, Alias) of
+        {ok, #{aliases:=Aliases}} -> Aliases;
+        {error, _} -> []
+    end.
+
+
+%% @doc Get's de pid for an object
+-spec get_pid(string()|binary()) ->
+    {ok, pid()} | {error, not_found|term()}.
+
+get_pid(UserObjId) ->
+    case resolve(UserObjId) of
+        {ok, _Class, _ObjId, Pid} ->
+            {ok, Pid};
+        {error, Error} ->
+            {error, Error}
+    end.
+
+
+%% @doc Gets a full object
+-spec get_obj(string()|binary()) ->
+    {ok, obj()} | {error, term()}.
+ 
+get_obj(UserObjId) ->
+    case resolve(UserObjId) of
+        {ok, {Class, ObjId, _Pid}} ->
+            case nkdomain_obj2:get_obj(Class, ObjId) of
+                {ok, Obj} ->
+                    {ok, Class, ObjId, Obj};
+                {error, Error} ->
+                    {error, Error}
+            end;
+        {error, Error} ->
+            {error, Error}
+    end.
+
+
+%% @doc Removes an object
+%% Most objects can also be removed loading a remove => true field
+-spec remove(string()|binary()) ->
+    ok | {error, term()}.
+
+remove(UserObjId) ->
+    case resolve(UserObjId) of
+        {ok, {Class, ObjId, _Pid}} ->
+            nkdomain_obj2:remove_obj(Class, ObjId);
+        {error, Error} ->
+            {error, Error}
+    end.
+
+
+%% @doc Gets an object's roles
+-spec get_roles(string()|binary()) ->
+    {ok, [nkrole:role()]} | {error, term()}.
+
+get_roles(UserObjId) ->
+    nkdomain_role:get_roles(UserObjId).
+
+
+%% @doc Gets object direct roles
+-spec get_role_objs(nkrole:role(), string()|binary()) ->
+    {ok, [nkrole:role_spec()]} | {error, term()}.
+
+get_role_objs(Role, UserObjId) ->
+    nkdomain_role:get_role_objs(Role, UserObjId).
+
+
+%% @doc Gets an object's roles iterating at all levels
+-spec find_role_objs(nkrole:role(), string()|binary()) ->
+    {ok, [obj_id()]} | {error, term()}.
+
+find_role_objs(Role, UserObjId) ->
+    nkdomain_role:find_role_objs(Role, UserObjId).
+
+
+%% @doc Check if Id has Role over Target
+-spec has_role(string()|binary(), nkrole:role(), string()|binary()) ->
+    {ok, boolean()} | {error, term()}.
+
+has_role(Id, Role, Target) ->
+    nkdomain_role:has_role(Id, Role, Target).
+
+
+%% @doc Resolves any id or alias
+-spec resolve(binary()|string()) ->
+    {ok, {class(), obj_id(), pid()}} | {error, term()}.
+
+resolve(UserObjId) ->
+    UserObjId1 = nklib_util:to_binary(UserObjId),
+    case get_aliases(UserObjId1) of
+        [] ->
+            nkdomain_util2:resolve(UserObjId1);
+        [UserObjId2] -> 
+            nkdomain_util2:resolve(UserObjId2);
+        _ ->
+            {error, multiple_aliases}
+    end.
+
+
+%% @doc Resolves any id or alias, allowing multiple alias
+-spec multi_resolve(binary()|string()) ->
+    [{class(), obj_id(), pid()}].
+
+multi_resolve(UserObjId) ->
+    UserObjId1 = nklib_util:to_binary(UserObjId),
+    case get_aliases(UserObjId1) of
+        [] ->
+            case nkdomain_util2:resolve(UserObjId1) of
+                {ok, Data} -> [Data];
+                {error, _} -> []
+            end;
+        UserObjIdList ->
+            lists:foldl(
+                fun(Id, Acc) ->
+                    case nkdomain_util2:resolve(Id) of
+                        {ok, {Class, ObjId, Pid}} -> 
+                            [{Class, ObjId, Pid}|Acc];
+                        {error, _} -> 
+                            Acc
+                    end
+                end,
+                [],
+                UserObjIdList)
+    end.
+
+
+
+%% @doc Registers a callback that will be called any time and object is updated
+%% as Mod:Fun(ObjId, Spec|removed)
+-spec register_update_callback(class(), atom(), atom()) ->
+    ok.
+
+register_update_callback(Class, Mod, Fun) when is_atom(Class) ->
+    code:ensure_loaded(Mod),
+    ok = nkdomain_app:put({update, Class}, {Mod, Fun}).
+
+
+
