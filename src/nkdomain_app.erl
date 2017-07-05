9206a7b3dcf17fced3a450115a985dea851462a8
Pedro Gutiérrez <pedrogutierrez@mac.com>
2017-07-05 23:02:34 +0200

    added call to nkdist:wait_for_service/0

--- src/nkdomain_app.erl
+++ src/nkdomain_app.erl
@@ -88,6 +88,8 @@ start(_Type, _Args) ->
     case nklib_config:load_env(?APP, Syntax) of
         {ok, _} ->
             {ok, Pid} = nkdomain_sup:start_link(),
+            lager:info("waiting for nkdist"),
+            nkdist:wait_for_service(),
             %% ok = riak_core_ring_events:add_guarded_handler(nkdomain_ring_handler, []),
             {ok, Vsn} = application:get_key(nkdomain, vsn),
             lager:info("NkDOMAIN v~s has started.", [Vsn]),
