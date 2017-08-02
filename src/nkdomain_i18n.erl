8509ec735b7666bffa779087adf5cbd4bfd6ab26
Valentín Adolfo Acosta García <valentin.acosta@jaraxa.net>
2017-08-02 18:22:05 +0200

    Removed a duplicated id

--- src/nkdomain_i18n.erl
+++ src/nkdomain_i18n.erl
@@ -53,7 +53,6 @@ i18n() -> #{
         "domain_tree_services" => "Services",
 
         "domain_column_pos" => "#",
-        "domain_column_id" => "ID",
         "domain_column_domain" => "DOMAIN",
         "domain_column_id" => "ID",
         "domain_column_name" => "NAME",
@@ -90,7 +89,6 @@ i18n() -> #{
         "domain_tree_services" => "Servicios",
 
         "domain_column_pos" => "#",
-        "domain_column_id" => "ID",
         "domain_column_domain" => "DOMINIO",
         "domain_column_id" => "ID",
         "domain_column_firstname" => "NOMBRE",
