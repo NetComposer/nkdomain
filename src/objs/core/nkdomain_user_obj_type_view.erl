90657fed69115f1534edb6afa76941b6bf36186f
Valentín Adolfo Acosta García <valentin.acosta@jaraxa.net>
2017-08-03 15:00:32 +0200

    Splitted user name column into two: Name y Last name

--- src/objs/core/nkdomain_user_obj_type_view.erl
+++ src/objs/core/nkdomain_user_obj_type_view.erl
@@ -70,7 +70,7 @@ view(Session) ->
             #{
                 id => name,
                 type => text,
-                header_colspan => 2,
+                header_colspan => 1,
                 filter_colspan => 2,
                 fillspace => <<"0.5">>,
                 name => domain_column_name,
@@ -80,6 +80,7 @@ view(Session) ->
             #{
                 id => surname,
                 type => text,
+                name => domain_column_lastname,
                 sort => true,
                 editor => text
             },
