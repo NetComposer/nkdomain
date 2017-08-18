a3bfc13c25647543de47952b70c21894c8d94bb5
Carlos Gonzalez <carlosj.gf@gmail.com>
2017-08-18 11:22:35 +0200

    Small fix
    Signed-off-by: Carlos Gonzalez <carlosj.gf@gmail.com>

--- src/objs/core/nkdomain_domain_obj.erl
+++ src/objs/core/nkdomain_domain_obj.erl
@@ -206,7 +206,7 @@ object_sync_op({nkdomain_reg_obj, ObjIdExt}, _From, #?STATE{id=Id}=State) ->
                 #?STATE{is_enabled=Enabled} = State,
                 {reply, {ok, Enabled, self()}, State4};
             {error, Error} ->
-                {error, Error}
+                {reply, {error, Error}, State}
         end;
     _ ->
         {reply, {error, object_path_invalid}, State}
