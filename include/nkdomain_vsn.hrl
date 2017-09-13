b87918e5f455eac22f177a251ea8d19533722650
Carlos Gonzalez <carlosj.gf@gmail.com>
2017-09-13 13:35:55 +0200

    Fix samples ports
    Signed-off-by: Carlos Gonzalez <carlosj.gf@gmail.com>

--- include/nkdomain_vsn.hrl
+++ include/nkdomain_vsn.hrl
@@ -25,8 +25,8 @@
 %% Defines
 %% ===================================================================
 
--define(HTTP, "https://127.0.0.1:9307/s/v07").
--define(WS, "wss://127.0.0.1:9307/s/v07/_api/ws").
+-define(HTTP, "https://127.0.0.1:9307/core/v07").
+-define(WS, "wss://127.0.0.1:9307/core/v07/_api/ws").
 
 %%-define(HTTP, "https://v1.netc.io/s/v07").
 %%-define(WS, "wss://v1.netc.io/s/v07/_api/ws").
