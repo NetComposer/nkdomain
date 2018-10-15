7081cf22cbc10c1c6575e81ea5647f5fba909930
Valentín Adolfo Acosta García <valentin.acosta@jaraxa.net>
2018-10-15 11:52:47 +0200

    Added create to nkdomain_mail_obj

--- src/objs/mail/nkdomain_mail_obj.erl
+++ src/objs/mail/nkdomain_mail_obj.erl
@@ -24,7 +24,7 @@
 -behavior(nkdomain_obj).
 -author('Carlos Gonzalez <carlosj.gf@gmail.com>').
 
--export([send_msg/3]).
+-export([create/4, send_msg/3]).
 -export([object_info/0, object_admin_info/0, object_parse/2, object_es_mapping/0]).
 
 -include_lib("nkdomain.hrl").
@@ -42,6 +42,39 @@
 %% ===================================================================
 %% API
 %% ===================================================================
+
+create(SrvId, Domain, UserId, Req) ->
+    From = maps:get(from, Req, <<>>),
+    To = maps:get(to, Req, <<>>),
+    Subject = maps:get(subject, Req, <<>>),
+    Body = maps:get(body, Req, <<>>),
+    ContentType = maps:get(content_type, Req, <<"text/plain">>),
+    MoreTags = maps:get(tags, Req, []),
+    Sent = maps:get(sent, Req, <<"false">>),
+    Tags = [
+        <<(?TAG_MAIL_FROM)/binary, ":", (nklib_util:to_binary(From))/binary>>,
+        <<(?TAG_MAIL_TO)/binary, ":", (nklib_util:to_binary(To))/binary>>,
+        <<(?TAG_MAIL_SUBJECT)/binary, ":", (nklib_util:to_binary(Subject))/binary>>,
+        <<(?TAG_MAIL_BODY)/binary, ":", (nklib_util:to_binary(Body))/binary>>,
+        <<(?TAG_MAIL_CONTENT_TYPE)/binary, ":", (nklib_util:to_binary(ContentType))/binary>>,
+        <<(?TAG_MAIL_SENT)/binary, ":", (nklib_util:to_binary(Sent))/binary>>
+        | MoreTags
+    ],
+    MailObj = #{
+        type => ?DOMAIN_MAIL,
+        srv_id => SrvId,
+        domain_id => Domain,
+        created_by => UserId,
+        name => Subject,
+        tags => Tags
+    },
+    case nkdomain_obj_make:create(MailObj) of
+        {ok, #obj_id_ext{obj_id=MailId}, []} ->
+            {ok, MailId};
+        {error, Error} -> 
+            {error, Error}
+    end.
+
 
 %% @doc Sends a mail msg
 -spec send_msg(nkservice:id(), nkdomain:id(), nkmail:msg()) ->
